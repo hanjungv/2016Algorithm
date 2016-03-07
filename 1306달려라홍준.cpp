cec89a9cf56ed77ebd51e85be5c6e8bf21a10dfe
Hanjung <hanjungv@gmail.com>
2016-03-07 19:45:33 +0900

    Update 1306달려라홍준.cpp

--- 1306달려라홍준.cpp
+++ 1306달려라홍준.cpp
@@ -32,13 +32,12 @@ int main(){
 	freopen("input.txt", "r", stdin);
 	scanf("%d %d", &N, &M);
 	tree.resize(4 * N);
-	for (idx = 1; idx < N; idx <<= 1);//base address ÃÊ±âÈ­
+	for (idx = 1; idx < N; idx <<= 1);//base address init
 	for (int i = 1; i <= N; i++){
 		scanf("%d", &num);
 		update(i, num);
 	}
-	for (int i = M; i <= N - M + 1 ; i++){
+	for (int i = M; i <= N - M + 1 ; i++)
 		printf("%d ", find(i - ( M - 1), i + M - 1));
-	}
 	return 0;
 }
